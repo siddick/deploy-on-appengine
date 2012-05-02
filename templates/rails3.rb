3860ba8da92fe001569a5aee6595fd5646121707
siddick <siddick@gmail.com>
2012-05-02 08:12:12 +0530

    change environment name to appengine

--- templates/rails3.rb
+++ templates/rails3.rb
@@ -10,8 +10,9 @@ end
 Gemfile
 
 append_file 'config/database.yml', <<-Database
+
 <% if defined? JRuby %>
-production:
+appengine:
   adapter: mysql
   driver: com.google.appengine.api.rdbms.AppEngineDriver
   url: jdbc:google:rdbms://username:username/dbname
@@ -23,18 +24,28 @@ if defined? JRuby
   ENV['BUNDLE_WITHOUT'] = 'development:test:extra'
   ENV['BUNDLE_PATH'] = 'file:WEB-INF/lib/gems.jar!/bundler_gems'
   ENV['BUNDLE_DISABLE_SHARED_GEMS'] = '1'
-  ENV['RAILS_ENV'] = 'production'
+  ENV['RAILS_ENV'] = 'appengine'
   $LOAD_PATH.push("file:WEB-INF/lib/gems.jar!/bundler/lib")
 
-  load File.expand_path("../lib/ruby_patch.rb", __FILE__)
+  load File.expand_path("../lib/jruby_patch.rb", __FILE__)
 end
 #{content}
 ConfigRu
 end
 
+create_file "config/environments/appengine.rb", File.read("config/environments/production.rb")
+gsub_file   "config/environments/appengine.rb", /Application.configure do/ do |content|
+<<-EnvConfig
+#{content}
+
+  require 'appengine-apis/logger'
+  config.logger = AppEngine::Logger.new
+EnvConfig
+end
+
 # run("gem install deploy-on-appengine")
 
-create_file "lib/ruby_patch.rb", <<-RubyPatch
+create_file "lib/jruby_patch.rb", <<-RubyPatch
 class File
   class << self
     alias :_org_join :join
